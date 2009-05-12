116d39b0cad7e35e2b236a51857a5fbd9d43dcd8
noamt <noamt@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2009-05-12 15:41:31 +0000

    Seperate snapshots test from simpleartifactorytest

--- public/trunk/itest/src/test/java/org/artifactory/test/internal/SimpleArtifactoryTest.java
+++ public/trunk/itest/src/test/java/org/artifactory/test/internal/SimpleArtifactoryTest.java
@@ -2,39 +2,30 @@ package org.artifactory.test.internal;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
-import org.artifactory.api.context.ArtifactoryContextThreadBinder;
 import org.artifactory.api.repo.RepoPath;
-import org.artifactory.api.request.DownloadService;
-import org.artifactory.engine.InternalUploadService;
-import org.artifactory.jcr.lock.InternalLockManager;
-import org.artifactory.jcr.lock.SessionLockEntry;
+import org.artifactory.descriptor.repo.VirtualRepoDescriptor;
 import org.artifactory.test.mock.MockPathTest;
 import org.artifactory.test.mock.MockTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.Resource;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 public class SimpleArtifactoryTest extends ArtifactoryTestBase {
     private final static Logger log = LoggerFactory.getLogger(SimpleArtifactoryTest.class);
     private static final String LOG4J_1_2_14_JAR = "log4j/log4j/1.2.14/log4j-1.2.14.jar";
+
+    @Override
+    String getConfigName() {
+        return "localhost-repo";
+    }
 
     @BeforeClass
     public void setLogLevel() {
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
         lc.getLogger(SimpleArtifactoryTest.class).setLevel(Level.DEBUG);
         //lc.getLogger(HttpRepo.class).setLevel(Level.DEBUG);
-        lc.getLogger(SessionLockEntry.class).setLevel(Level.TRACE);
-        lc.getLogger(InternalLockManager.class).setLevel(Level.TRACE);
-
         MockTest test = new MockTest("momo");
         test.addPath(new MockPathTest(LOG4J_1_2_14_JAR, "application/octet-stream", "/mock/repo1/" + LOG4J_1_2_14_JAR));
         mockServer.addTest(test);
@@ -43,10 +34,8 @@ public class SimpleArtifactoryTest extends ArtifactoryTestBase {
     @Test(invocationCount = 60, threadPoolSize = 20)
     public void simpleJarDownload() throws Exception {
         log.debug("Thread started concurrent simpleJarDownload");
-        DownloadService downloadService = context.beanForType(DownloadService.class);
-        ArtifactoryRequestStub request = new ArtifactoryRequestStub(LOG4J_1_2_14_JAR);
-        ArtifactoryResponseStub response = new ArtifactoryResponseStub();
-        downloadService.process(request, response);
+        ArtifactoryResponseStub response =
+                download(new RepoPath(VirtualRepoDescriptor.GLOBAL_VIRTUAL_REPO_KEY, LOG4J_1_2_14_JAR));
         Assert.assertTrue(response.isSuccessful(),
                 "Failure status: " + response.getStatus() + " reason:" + response.getReason());
         Assert.assertTrue(response.getContentLength() > 0);
@@ -64,71 +53,5 @@ public class SimpleArtifactoryTest extends ArtifactoryTestBase {
     public void simpleSnapshotUpload() throws Exception {
         RepoPath repoPath = new RepoPath("ext-snapshots-local", "test/test/1.0-SNAPSHOT/test-1.0-SNAPSHOT.jar");
         upload(repoPath, "resources/test.jar");
-    }
-
-    @Test(invocationCount = 20, threadPoolSize = 1,
-            description = "Mimic maven parallel uploading of pom + snapshot version metadata, in order to check " +
-                    "dealock handling.")
-    public void concurrentSnapshotPartsUpload() throws Exception {
-        Callable<?> pom1Call = new Callable() {
-            public String call() throws Exception {
-                log.info("........... Deploying POM1");
-                bindThreadContext();
-                ArtifactoryContextThreadBinder.bind(context);
-                RepoPath repoPath =
-                        new RepoPath("ext-snapshots-local", "test-group/test/1.0-SNAPSHOT/test-20090511.121120-2.pom");
-                upload(repoPath, "resources/test-1.0-SNAPSHOT.pom");
-                return null;
-            }
-        };
-        Callable<?> pom2Call = new Callable() {
-            public String call() throws Exception {
-                log.info("........... Deploying POM2");
-                bindThreadContext();
-                ArtifactoryContextThreadBinder.bind(context);
-                RepoPath repoPath =
-                        new RepoPath("ext-snapshots-local", "test-group/test/1.0-SNAPSHOT/test-20090511.121120-2.pom");
-                upload(repoPath, "resources/test-1.0-SNAPSHOT.pom");
-                return null;
-            }
-        };
-        Callable<?> metadataCall = new Callable<String>() {
-            public String call() throws Exception {
-                log.info("........... Deploying MAVEN-METADATA");
-                bindThreadContext();
-                RepoPath repoPath =
-                        new RepoPath("ext-snapshots-local", "test-group/test/1.0-SNAPSHOT/maven-metadata.xml");
-                upload(repoPath, "resources/test-1.0-SNAPSHOT-maven-metadata.xml");
-                return null;
-            }
-        };
-
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        Future<?> result1 = executorService.submit(pom1Call);
-        Future<?> result2 = executorService.submit(metadataCall);
-        try {
-            result1.get();
-            result2.get();
-        } catch (Exception e) {
-            Assert.fail("Snapshot upload failed!", e.getCause());
-        }
-    }
-
-    private void upload(RepoPath repoPath, String classpathRes) throws IOException {
-        InternalUploadService uploadService = context.beanForType(InternalUploadService.class);
-        ArtifactoryRequestStub request = new ArtifactoryRequestStub(repoPath.getRepoKey(), "/" + repoPath.getPath());
-        Resource resource = context.getResource("classpath:" + classpathRes);
-        request.setInputStream(resource.getInputStream());
-        ArtifactoryResponseStub response = new ArtifactoryResponseStub();
-
-        uploadService.process(request, response);
-
-        Assert.assertTrue(response.isSuccessful(),
-                "Failure status: " + response.getStatus() + " reason:" + response.getReason() + " for " + repoPath);
-    }
-
-    @Override
-    String getConfigName() {
-        return "localhost-repo";
     }
 }
