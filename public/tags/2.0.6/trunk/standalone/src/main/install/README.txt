24a4c983fb2e995171a22f29569af1fd532eae9d
bartender <bartender@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2009-05-22 19:45:22 +0000

    oops

--- public/tags/2.0.6/trunk/standalone/src/main/install/README.txt
+++
@@ -1,7 +0,0 @@
-For more information about Artifactory please visit:
-
-   http://artifactory.jfrog.org
-
-The default administrator user is:
-username: admin
-password: password
--- public/tags/2.0.6/trunk/webapp/src/main/java/org/artifactory/webapp/wicket/common/component/modal/HasModalHandler.java
+++
@@ -1,8 +0,0 @@
-package org.artifactory.webapp.wicket.common.component.modal;
-
-/**
- * @author Yoav Aharoni
- */
-public interface HasModalHandler {
-    ModalHandler getModalHandler();
-}
