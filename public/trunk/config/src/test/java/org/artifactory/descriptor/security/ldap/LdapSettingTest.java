8710b08b9116cf2efba2a05392fb3be7ada56073
noamt <noamt@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2009-08-05 07:34:12 +0000

    RTFACT-2036 - Exception when using capital letters in the ldap protocol

--- public/trunk/config/src/test/java/org/artifactory/descriptor/security/ldap/LdapSettingTest.java
+++ public/trunk/config/src/test/java/org/artifactory/descriptor/security/ldap/LdapSettingTest.java
@@ -20,4 +20,38 @@ public class LdapSettingTest {
         Assert.assertNull(ldap.getSearch());
     }
 
+    /**
+     * Tests the URL protocol lower-case transformer
+     */
+    public void testUrlProtocolTransformer() {
+        testLdapUrl("", "", "Empty LDAP URL should have remained unchanged");
+
+        testLdapUrl("invalidLDAPURL", "invalidLDAPURL", "Inavlid LDAP URL should have remained unchanged");
+
+        testLdapUrl("LDAP://url", "ldap://url", "Upper-cased LDAP URL should have been transformed to lower case");
+
+        testLdapUrl("LDAP://URL", "ldap://URL", "Upper-cased LDAP URL should have been transformed to lower case");
+
+        testLdapUrl("ldap://url", "ldap://url", "Valid LDAP URL should have remained unchanged");
+
+        testLdapUrl("ldap://URL", "ldap://URL", "Valid LDAP URL should have remained unchanged");
+
+        testLdapUrl("ldap://URL:30/moo=MOO", "ldap://URL:30/moo=MOO", "Valid LDAP URL should have remained unchanged");
+
+        testLdapUrl("ldap://URL:30/moo=MOO,maa=MAA", "ldap://URL:30/moo=MOO,maa=MAA",
+                "Valid LDAP URL should have remained unchanged");
+    }
+
+    /**
+     * Tests the LDAP URL getter & setter (needed since URL is being transformed)
+     *
+     * @param inputUrl     URL to supply to setter
+     * @param outputUrl    URL to compare with results from getter
+     * @param errorMessage Message to print on unexpected result
+     */
+    private void testLdapUrl(String inputUrl, String outputUrl, String errorMessage) {
+        LdapSetting ldap = new LdapSetting();
+        ldap.setLdapUrl(inputUrl);
+        Assert.assertEquals(ldap.getLdapUrl(), outputUrl, errorMessage);
+    }
 }
