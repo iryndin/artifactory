9a59aebeef4ffaa2ef2e8bbdd94879b02b781b33
yossis <yossis@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2010-02-09 11:02:34 +0000

    Updating trunk to version 2.2.0

---
+++ cli/src/main/java/org/artifactory/cli/common/OptionParser.java
@@ -0,0 +1,33 @@
+/*
+ * Artifactory is a binaries repository manager.
+ * Copyright (C) 2010 JFrog Ltd.
+ *
+ * Artifactory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Artifactory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Artifactory.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.artifactory.cli.common;
+
+/**
+ * @author freds
+ * @date Sep 1, 2008
+ */
+public abstract class OptionParser {
+    /**
+     * You never return from usage since it throws System.exit(). But for good static analysis may be Neal is rigth
+     * about the Nothing type?
+     */
+    public abstract void usage();
+
+
+}
