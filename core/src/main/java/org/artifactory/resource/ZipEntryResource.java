9bebc4145095b398eb9f047f2b46666b86f2d32f
elig <elig@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2011-08-01 14:25:20 +0000

    Updating trunk to version 2.3.4.1

---
+++ core/src/main/java/org/artifactory/resource/ZipEntryResource.java
@@ -0,0 +1,90 @@
+/*
+ * Artifactory is a binaries repository manager.
+ * Copyright (C) 2011 JFrog Ltd.
+ *
+ * Artifactory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Artifactory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Artifactory.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.artifactory.resource;
+
+import org.artifactory.api.fs.RepoResource;
+import org.artifactory.api.fs.ZipEntryResourceInfo;
+import org.artifactory.repo.RepoPath;
+
+/**
+ * This downloadable resource represents a file inside a zip/jar resource.
+ * <p/>
+ *
+ * @author Yossi Shaul
+ */
+public class ZipEntryResource implements RepoResource {
+    private final ZipEntryResourceInfo info;
+    private RepoPath responsePath;
+
+    public ZipEntryResource(ZipEntryResourceInfo zipInfo) {
+        this.info = zipInfo;
+    }
+
+    public String getEntryName() {
+        return info.getEntryName();
+    }
+
+    public RepoPath getRepoPath() {
+        return info.getRepoPath();
+    }
+
+    public RepoPath getResponseRepoPath() {
+        return responsePath != null ? responsePath : getRepoPath();
+    }
+
+    public void setResponseRepoPath(RepoPath responsePath) {
+        this.responsePath = responsePath;
+    }
+
+    public RepoResourceInfo getInfo() {
+        return info;
+    }
+
+    public boolean isFound() {
+        return true;
+    }
+
+    public boolean isExactQueryMatch() {
+        return true;
+    }
+
+    public boolean isExpired() {
+        return false;
+    }
+
+    public boolean isMetadata() {
+        return false;
+    }
+
+    public long getSize() {
+        return info.getSize();
+    }
+
+    public long getCacheAge() {
+        return info.getAge();
+    }
+
+    public long getLastModified() {
+        return info.getLastModified();
+    }
+
+    public String getMimeType() {
+        return info.getMimeType();
+    }
+}
