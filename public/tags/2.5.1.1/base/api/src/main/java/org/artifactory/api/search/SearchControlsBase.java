9d90901caaea311b39f6206ff37614ede3d8d07f
elig <elig@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2012-03-06 12:18:32 +0000

    Tagging version 2.5.1.1

---
+++ public/tags/2.5.1.1/base/api/src/main/java/org/artifactory/api/search/SearchControlsBase.java
@@ -0,0 +1,74 @@
+/*
+ * Artifactory is a binaries repository manager.
+ * Copyright (C) 2012 JFrog Ltd.
+ *
+ * Artifactory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Artifactory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Artifactory.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.artifactory.api.search;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * A base implementation for all search control classes
+ *
+ * @author Noam Y. Tenne
+ */
+public abstract class SearchControlsBase implements SearchControls {
+
+    private boolean limitSearchResults = true;
+    protected List<String> selectedRepoForSearch;
+
+    @Override
+    public List<String> getSelectedRepoForSearch() {
+        return selectedRepoForSearch;
+    }
+
+    public void setSelectedRepoForSearch(List<String> selectedRepoForSearch) {
+        this.selectedRepoForSearch = selectedRepoForSearch;
+    }
+
+    @Override
+    public boolean isLimitSearchResults() {
+        return limitSearchResults;
+    }
+
+    /**
+     * Sets the search result limit indicator
+     *
+     * @param limitSearchResults True if the search results should be limited
+     */
+    public void setLimitSearchResults(boolean limitSearchResults) {
+        this.limitSearchResults = limitSearchResults;
+    }
+
+    @Override
+    public void resetResultLimit() {
+        limitSearchResults = true;
+    }
+
+    @Override
+    public boolean isSpecificRepoSearch() {
+        return selectedRepoForSearch != null && !selectedRepoForSearch.isEmpty();
+    }
+
+    public void addRepoToSearch(String repoKey) {
+        if (selectedRepoForSearch == null) {
+            selectedRepoForSearch = Lists.newArrayList();
+        }
+        selectedRepoForSearch.add(repoKey);
+    }
+}
