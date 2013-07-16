089a6cdf6c11e4e1bb393935b015d9cfbba91b7b
oferh <oferh@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2013-07-16 08:36:13 +0000

    Updating trunk to version 3.0.2.1

--- public/trunk/distribution/standalone/src/main/install/bin/uninstallService.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-set SERVICE_NAME=Artifactory
-set EXECUTABLE=..\bin\artifactory-service.exe
-
-"%EXECUTABLE%" //DS//%SERVICE_NAME%
-echo The service '%SERVICE_NAME%' has been removed
