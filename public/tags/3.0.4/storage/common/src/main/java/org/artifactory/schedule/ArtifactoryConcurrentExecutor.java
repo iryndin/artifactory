d4f08d4f6c2429ef7f75c05526aa14995095eba1
elig <elig@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2013-10-27 16:17:32 +0000

    Tagging version 3.0.4

---
+++ public/tags/3.0.4/storage/common/src/main/java/org/artifactory/schedule/ArtifactoryConcurrentExecutor.java
@@ -0,0 +1,135 @@
+/*
+ * Artifactory is a binaries repository manager.
+ * Copyright (C) 2012 JFrog Ltd.
+ *
+ * Artifactory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Artifactory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Artifactory.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.artifactory.schedule;
+
+import org.artifactory.api.context.ArtifactoryContextThreadBinder;
+import org.artifactory.common.ArtifactoryHome;
+import org.artifactory.common.ConstantValues;
+import org.artifactory.storage.spring.ArtifactoryStorageContext;
+import org.artifactory.storage.spring.StorageContextHelper;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Yossi Shaul
+ * @author Fred Simon
+ */
+public class ArtifactoryConcurrentExecutor implements Executor {
+    private final ArtifactoryStorageContext storageContext;
+    private final ThreadPoolExecutor executor;
+
+    ArtifactoryConcurrentExecutor() {
+        CustomizableThreadFactory threadFactory = new CustomizableThreadFactory("art-exec-");
+        threadFactory.setThreadPriority(Thread.NORM_PRIORITY);
+        executor = new ThreadPoolExecutor(
+                ConstantValues.asyncCorePoolSize.getInt(),
+                ConstantValues.asyncCorePoolSize.getInt(),
+                ConstantValues.asyncPoolTtlSecs.getInt(),
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(ConstantValues.asyncPoolMaxQueueSize.getInt()),
+                threadFactory);
+        executor.allowCoreThreadTimeOut(true);
+        storageContext = StorageContextHelper.get();
+    }
+
+    @Override
+    public void execute(Runnable task) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        task = new RunnableWrapper(task, authentication);
+        executor.execute(task);
+    }
+
+    <T> Future<T> submit(Runnable task, T result) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        task = new RunnableWrapper(task, authentication);
+        return executor.submit(task, result);
+    }
+
+    void shutdown() {
+        executor.shutdown();
+    }
+
+    class RunnableWrapper implements Runnable {
+        private final Runnable delegate;
+        private final Authentication authentication;
+
+
+        RunnableWrapper(Runnable delegate, Authentication authentication) {
+            this.delegate = delegate;
+            this.authentication = authentication;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ArtifactoryContextThreadBinder.bind(storageContext);
+                ArtifactoryHome.bind(storageContext.getArtifactoryHome());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                delegate.run();
+            } finally {
+                // in case an async operation is fired while shutdown (i.e gc) the context holder strategy is
+                // cleared and NPE can happen after the async finished (or is finishing). see RTFACT-2812
+                if (storageContext.isReady()) {
+                    SecurityContextHolder.clearContext();
+                }
+                ArtifactoryContextThreadBinder.unbind();
+                ArtifactoryHome.unbind();
+            }
+        }
+    }
+
+    public int getActiveCount() {
+        return executor.getActiveCount();
+    }
+
+    public long getCompletedTaskCount() {
+        return executor.getCompletedTaskCount();
+    }
+
+    public int getCorePoolSize() {
+        return executor.getCorePoolSize();
+    }
+
+    public int getLargestPoolSize() {
+        return executor.getLargestPoolSize();
+    }
+
+    public int getMaximumPoolSize() {
+        return executor.getMaximumPoolSize();
+    }
+
+    public long getTaskCount() {
+        return executor.getTaskCount();
+    }
+
+    public void setCorePoolSize(int corePoolSize) {
+        executor.setCorePoolSize(corePoolSize);
+    }
+
+    public void setMaximumPoolSize(int maximumPoolSize) {
+        executor.setMaximumPoolSize(maximumPoolSize);
+    }
+}
