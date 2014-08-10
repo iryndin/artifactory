3134b6a355b439f75eeadf9cfe942f0a6c23cf56
elig <elig@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2014-08-10 16:13:15 +0000

    Tagging version 3.3.0.1

---
+++ public/tags/3.3.0.1/storage/db/src/main/java/org/artifactory/storage/db/fs/util/PropertiesFilterQueryBuilder.java
@@ -0,0 +1,111 @@
+/*
+ * Artifactory is a binaries repository manager.
+ * Copyright (C) 2014 JFrog Ltd.
+ *
+ * Artifactory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Artifactory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Artifactory.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.artifactory.storage.db.fs.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.artifactory.api.properties.PropertiesFilter;
+import org.artifactory.util.Pair;
+
+/**
+ * Helper to create properties filter query
+ *
+ * @author Gidi Shabat
+ */
+public class PropertiesFilterQueryBuilder {
+
+    private PropertiesFilter propertiesFilter;
+
+    public PropertiesFilterQueryBuilder(PropertiesFilter propertiesFilter) {
+        this.propertiesFilter = propertiesFilter;
+    }
+
+    public String createDistinctFirst() {
+        String query = "SELECT distinct p1.prop_value FROM nodes n <PROPERTIES_FILTER> <PATH_FILTER> ";
+        query = query.replace("<PATH_FILTER>", createPathFilter());
+        query = query.replace("<PROPERTIES_FILTER>", nodeIdsFilter1());
+        return query;
+    }
+
+    public String createDistinctVersionQuery() {
+        String distinct = propertiesFilter.getDistinct();
+        if (StringUtils.isNotEmpty(distinct)) {
+            propertiesFilter.addFirst(distinct);
+        }
+        String query = "SELECT distinct p1.prop_value FROM nodes n <PROPERTIES_FILTER> <PATH_FILTER> ";
+        query = query.replace("<PATH_FILTER>", createPathFilter());
+        query = query.replace("<PROPERTIES_FILTER>", nodeIdsFilter1());
+        return query;
+    }
+
+    public String createNodeQuery() {
+        String query = "SELECT n.* FROM nodes n <PROPERTIES_FILTER> <PATH_FILTER>";
+        query = query.replace("<PATH_FILTER>", createPathFilter());
+        query = query.replace("<PROPERTIES_FILTER>", nodeIdsFilter1());
+        return query;
+    }
+
+    private String createPathFilter() {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (StringUtils.isNotEmpty(propertiesFilter.getRepo())) {
+            stringBuilder.append(" AND repo ='");
+            stringBuilder.append(propertiesFilter.getRepo());
+            stringBuilder.append("'");
+        }
+        if (StringUtils.isNotEmpty(propertiesFilter.getPath())) {
+            stringBuilder.append(" AND node_path like '");
+            stringBuilder.append(propertiesFilter.getPath());
+            stringBuilder.append("%'");
+        }
+        return stringBuilder.toString();
+    }
+
+    private String nodeIdsFilter1() {
+        StringBuilder total = new StringBuilder();
+        StringBuilder join = new StringBuilder();
+        StringBuilder where = new StringBuilder();
+        propertiesFilter.getProperties().iterator();
+        where.append(" WHERE node_type = 1");
+        for (int i = 0; i < propertiesFilter.getProperties().size(); i++) {
+            Pair<String, String> pair = propertiesFilter.getProperties().get(i);
+            String indexValue = "p" + (i + 1);
+            join.append("JOIN node_props ");
+            join.append(indexValue);
+            join.append(" ON n.node_id = ");
+            join.append(indexValue);
+            join.append(".node_id ");
+            where.append(" AND ");
+            where.append(indexValue);
+            where.append(".prop_key='");
+            where.append(pair.getFirst());
+            where.append("' ");
+            String value = pair.getSecond();
+            if (value != null) {
+                where.append(" AND ");
+                where.append(indexValue);
+                where.append(".prop_value='");
+                where.append(value);
+                where.append("' ");
+            }
+        }
+
+        total.append(join.toString());
+        total.append(where.toString());
+        return total.toString();
+    }
+}
---
+++ public/tags/3.3.0.1/web/rest/src/main/java/org/artifactory/rest/resource/archive/ArchiveResource.java
@@ -0,0 +1,177 @@
+/*
+ * Artifactory is a binaries repository manager.
+ * Copyright (C) 2012 JFrog Ltd.
+ *
+ * Artifactory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Artifactory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Artifactory.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.artifactory.rest.resource.archive;
+
+import com.sun.jersey.spi.CloseableService;
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpStatus;
+import org.artifactory.addon.AddonsManager;
+import org.artifactory.addon.rest.AuthorizationRestException;
+import org.artifactory.addon.rest.RestAddon;
+import org.artifactory.api.repo.RepositoryService;
+import org.artifactory.api.rest.build.artifacts.BuildArtifactsRequest;
+import org.artifactory.api.rest.constant.ArchiveRestConstants;
+import org.artifactory.api.rest.constant.BuildRestConstants;
+import org.artifactory.api.security.AuthorizationService;
+import org.artifactory.mime.MimeType;
+import org.artifactory.mime.NamingUtils;
+import org.artifactory.repo.RepoPath;
+import org.artifactory.rest.common.exception.BadRequestException;
+import org.artifactory.rest.common.exception.NotFoundException;
+import org.artifactory.rest.util.RestUtils;
+import org.artifactory.search.archive.InternalArchiveIndexer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.commons.lang.StringUtils.isBlank;
+
+/**
+ * Resource class which handles archive operations
+ *
+ * @author Shay Yaakov
+ */
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Path(ArchiveRestConstants.PATH_ROOT)
+@RolesAllowed({AuthorizationService.ROLE_USER, AuthorizationService.ROLE_ADMIN})
+public class ArchiveResource {
+    private static final Logger log = LoggerFactory.getLogger(ArchiveResource.class);
+
+    @Context
+    private CloseableService closeableService;
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Autowired
+    private AddonsManager addonsManager;
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Autowired
+    private InternalArchiveIndexer archiveIndexer;
+
+    @POST
+    @Path(ArchiveRestConstants.PATH_BUILD_ARTIFACTS)
+    @Consumes({BuildRestConstants.MT_BUILD_ARTIFACTS_REQUEST, MediaType.APPLICATION_JSON})
+    public Response getBuildArtifactsArchive(BuildArtifactsRequest buildArtifactsRequest) throws IOException {
+        if (isBlank(buildArtifactsRequest.getBuildName())) {
+            throw new BadRequestException("Cannot search without build name.");
+        }
+        boolean buildNumberIsBlank = isBlank(buildArtifactsRequest.getBuildNumber());
+        boolean buildStatusIsBlank = isBlank(buildArtifactsRequest.getBuildStatus());
+        if (buildNumberIsBlank && buildStatusIsBlank) {
+            throw new BadRequestException("Cannot search without build number or build status.");
+        }
+        if (!buildNumberIsBlank && !buildStatusIsBlank) {
+            throw new BadRequestException("Cannot search with both build number and build status parameters, " +
+                    "please omit build number if your are looking for latest build by status " +
+                    "or omit build status to search for specific build version.");
+        }
+
+        if (buildArtifactsRequest.getArchiveType() == null) {
+            throw new BadRequestException("Archive type cannot be empty, please provide a type of zip/tar/tar.gz/tgz.");
+        }
+
+        if (!authorizationService.isAuthenticated()) {
+            throw new AuthorizationRestException();
+        }
+
+        RestAddon restAddon = addonsManager.addonByType(RestAddon.class);
+        try {
+            final File buildArtifactsArchive = restAddon.getBuildArtifactsArchive(buildArtifactsRequest);
+            if (buildArtifactsArchive == null) {
+                throw new NotFoundException(
+                        String.format("Could not find any build artifacts for build '%s' number '%s'.",
+                                buildArtifactsRequest.getBuildName(),
+                                buildArtifactsRequest.getBuildNumber()));
+            }
+
+            markForDeletionAtResponseEnd(buildArtifactsArchive);
+
+            MimeType mimeType = NamingUtils.getMimeType(buildArtifactsArchive.getName());
+            return Response.ok().entity(buildArtifactsArchive).type(mimeType.getType()).build();
+        } catch (IOException e) {
+            log.error("Failed to create builds artifacts archive: " + e.getMessage(), e);
+            throw new NotFoundException("Failed to create builds artifacts archive");
+        }
+    }
+
+    @POST
+    @Path(ArchiveRestConstants.PATH_INDEX)
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed({AuthorizationService.ROLE_ADMIN})
+    public Response index(@QueryParam("path") String path, @QueryParam("recursive") int recursive,
+            @QueryParam("indexAllRepos") int indexAllRepos) {
+
+        RepoPath repoPath = null;
+        if (indexAllRepos != 1) {
+            repoPath = RestUtils.calcRepoPathFromRequestPath(path);
+            if (!repositoryService.exists(repoPath)) {
+                return Response.status(Response.Status.BAD_REQUEST).entity("Could not find repo path " + path).build();
+            }
+        }
+
+        if (indexAllRepos != 1 && recursive == 0) {
+            archiveIndexer.markArchiveForIndexing(repoPath);
+        } else {
+            archiveIndexer.recursiveMarkArchivesForIndexing(repoPath, indexAllRepos == 1);
+        }
+
+        //trigger async archive indexing now
+        archiveIndexer.asyncIndexMarkedArchives();
+
+        String message;
+        if (repoPath != null) {
+            message = "Archive indexing for path '" + path + "' accepted.";
+        } else {
+            message = "Archive indexing of all repositories accepted.";
+        }
+        log.info(message);
+        return Response.status(HttpStatus.SC_ACCEPTED).entity(message).build();
+    }
+
+    private void markForDeletionAtResponseEnd(final File buildArtifactsArchive) {
+        // delete the file after jersey streamed it back to the client
+        closeableService.add(new Closeable() {
+            @Override
+            public void close() throws IOException {
+                FileUtils.deleteQuietly(buildArtifactsArchive);
+            }
+        });
+    }
+}
