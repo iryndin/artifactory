a5e5941d2514efc07eea33ae9a7a55a71cb368c5
oferh <oferh@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2015-07-06 14:32:13 +0000

    Updating trunk to version 3.9.2

---
+++ web/rest/src/main/java/org/artifactory/rest/services/config/RestServiceConfiguration.java
@@ -0,0 +1,13 @@
+package org.artifactory.rest.services.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author Chen Keinan
+ */
+@Configuration
+@ImportResource({"classpath:/META-INF/spring/restContext.xml"})
+public class RestServiceConfiguration {
+
+}
