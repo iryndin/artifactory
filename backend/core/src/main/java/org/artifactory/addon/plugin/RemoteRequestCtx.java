d9aceac5b65adbf5c3d332d5f7b00c6130d4c553
oferh <oferh@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2015-08-13 08:54:32 +0000

    Updating trunk to version 3.9.4

---
+++ backend/core/src/main/java/org/artifactory/addon/plugin/RemoteRequestCtx.java
@@ -0,0 +1,20 @@
+package org.artifactory.addon.plugin;
+
+import org.artifactory.addon.plugin.download.ResourceStreamCtx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Gidi Shabat
+ */
+public class RemoteRequestCtx extends ResourceStreamCtx {
+
+    private Map<String, String> headers = new HashMap<>();
+
+    public Map<String, String> getHeaders() {return headers;}
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+}
