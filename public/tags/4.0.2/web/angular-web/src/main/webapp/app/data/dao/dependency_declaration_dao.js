a0b1bb10b3fab5ee71a215e17cf1f461b039d66a
oferh <oferh@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2015-08-13 08:28:38 +0000

    Rmoving tag 4.0.2

--- public/tags/4.0.2/web/angular-web/src/main/webapp/app/data/dao/dependency_declaration_dao.js
+++
@@ -1,8 +0,0 @@
-import {ArtifactoryDao} from '../artifactory_dao';
-
-export function DependencyDeclarationDao(RESOURCE, ArtifactoryDaoFactory) {
-    return ArtifactoryDaoFactory()
-            .setDefaults({method: 'POST'})
-            .setPath(RESOURCE.DEPENDENCY_DECLARATION)
-            .getInstance();
-}
