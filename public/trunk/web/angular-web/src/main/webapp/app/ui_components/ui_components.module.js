72c74544a37d8ec2e3aaecadbafb0855450518e6
oferh <oferh@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2015-09-08 13:39:16 +0000

    Updating trunk to version 4.1.0

--- public/trunk/web/angular-web/src/main/webapp/app/ui_components/ui_components.module.js
+++
@@ -1,7 +0,0 @@
-import Grid from './artifactory_grid/artifactory_grid.module';
-import Modal from './artifactory_modal/artifactory_modal.module';
-
-angular.module('artifactory.ui_components', [
-    Grid.name,
-    Modal.name
-]);
