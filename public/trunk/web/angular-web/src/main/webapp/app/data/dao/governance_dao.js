770935cabb70512a1a1c7efc3e3c4462b0e5ff58
oferh <oferh@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2015-09-20 18:09:21 +0000

    Updating trunk to version 4.1.2

--- public/trunk/web/angular-web/src/main/webapp/app/data/dao/governance_dao.js
+++
@@ -1,8 +0,0 @@
-import {ArtifactoryDao} from '../artifactory_dao';
-
-export function GovernanceDao(RESOURCE, ArtifactoryDaoFactory) {
-    return ArtifactoryDaoFactory()
-        .setDefaults({method: 'POST'})
-        .setPath(RESOURCE.ARTIFACT_GOVERNANCE)
-        .getInstance();
-}
