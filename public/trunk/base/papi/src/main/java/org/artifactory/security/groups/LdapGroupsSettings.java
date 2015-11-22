75d5dd7af6d02826da76526379a190d26f15dd8a
oferh <oferh@2c6c46e5-621b-0410-92f3-c2a4854cd828>
2015-11-22 15:51:37 +0000

    Updating trunk to version 4.3.0

--- public/trunk/base/papi/src/main/java/org/artifactory/security/groups/LdapGroupsSettings.java
+++
@@ -1,9 +0,0 @@
-package org.artifactory.security.groups;
-
-/**
- * @author Shay Yaakov
- */
-public class LdapGroupsSettings implements RealmAwareGroupsSettings {
-
-    public String ldapGroupSettingsName;
-}
--- public/trunk/web/angular-web/src/main/webapp/app/states/artifacts/jf_stash_browser/jf_stash_browser.config.js
+++
@@ -1,4 +0,0 @@
-import TreeConfig from '../jf_common_browser/jf_common_browser.config';
-let conf = _.cloneDeep(TreeConfig);
-_.extend(conf.core,{'check_callback':true});
-export default conf;
